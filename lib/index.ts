4a3218c26e498ba52fba58c133bcfae5b3df7ecb
Nitin <nitinjain7201@gmail.com>
2024-11-24 16:42:44 +0530

    Management Portal

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 let client: MongoClient;
 
-const url = process.env.MONGO_URL
+const url = 'mongodb+srv://nj5930595:YCORpV7AMRZF3soD@fomofactory.kas6lb3.mongodb.net/';
 
 async function connectDatabase() {
     if (!client) {
